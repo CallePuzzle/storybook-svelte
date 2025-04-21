9ba05b68a0ee851e874c29c791238bbde9a93e04
César M. Cristóbal <cesar@callepuzzle.com>
2025-04-21 21:11:36 +0200

    supabase layout server

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
 	import '../app.css';
+	import { defaults } from 'sveltekit-superforms/client';
+	import { zod } from 'sveltekit-superforms/adapters';
 	import Header from '$lib/components/Header.svelte';
 	import { routes } from '$lib/routes.js';
+	import { loginSchema } from '$lib/schemas/login.js';
 
 	let { children } = $props();
+
+	const formValidated = defaults({ email: '' }, zod(loginSchema));
 </script>
 
-<Header title="NavNar Title" {routes}>
+<Header title="NavNar Title" {routes} {formValidated}>
 	{@render children()}
 </Header>
