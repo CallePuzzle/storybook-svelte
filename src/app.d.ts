ce4fac4443fcedca039aed109a21e0c812f525f9
César M. Cristóbal <cesar@callepuzzle.com>
2025-04-21 19:53:42 +0200

    supabase handle

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,7 +1,19 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
+//
+import type { Session, SupabaseClient, User } from '@supabase/supabase-js';
+
 declare global {
 	namespace App {
+		interface Locals {
+			supabase: SupabaseClient;
+			safeGetSession: () => Promise<{ session: Session | null; user: User | null }>;
+			session: Session | null;
+			user: User | null;
+		}
+		interface PageData {
+			session: Session | null;
+		}
 		// interface Error {}
 		// interface Locals {}
 		// interface PageData {}
