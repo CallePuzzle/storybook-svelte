a8d23003c3a711094eb4be005b92ac722a75cd51
César M. Cristóbal <cesar@callepuzzle.com>
2025-07-21 18:30:59 +0200

    fix(demo sveltekit): montando demo

---
+++ demos/sveltekit/src/lib/routes.ts
@@ -0,0 +1,25 @@
+import { m } from './paraglide/messages.js';
+import { routes as coreRoutes, type Routes } from 'storybook-svelte';
+
+const myRoutes: Routes = {
+	org_list: {
+		name: m.routes_org_list(),
+		url: '/org/',
+		isProtected: true,
+		showInMenu: true
+	},
+	org_index: {
+		name: m.routes_org_index(),
+		url: (id: string) => `/org/${id}`,
+		isProtected: true,
+		showInMenu: false
+	},
+	grocery_index: {
+		name: m.routes_grocery_index(),
+		url: (id: string) => `/org/${id}/grocery`,
+		isProtected: true,
+		showInMenu: false
+	}
+};
+
+export const routes = { ...coreRoutes, ...myRoutes };
---
+++ demos/sveltekit/src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import "../app.css";
+  import { Header } from "@repo/sveltekit-ui";
+  import { routes } from "$lib/routes.js";
+  import { session, authClient } from "$lib/auth-client";
+
+  import type { PageData } from "./$types";
+  import type { Snippet } from "svelte";
+
+  let { children, data }: { children: Snippet; data: PageData } = $props();
+</script>
+
+<Header title="NavNar Title" {routes} {session} {authClient}>
+  {@render children()}
+</Header>
