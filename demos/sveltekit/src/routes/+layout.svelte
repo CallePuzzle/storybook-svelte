ac5640bc382157c3a408db4ccf01bf441db45dad
César M. Cristóbal <cesar@callepuzzle.com>
2025-08-02 13:18:23 +0200

    improve paraglide config

--- demos/sveltekit/src/routes/+layout.svelte
+++ demos/sveltekit/src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
   import "../app.css";
+  import { page } from "$app/state";
+  import { locales, localizeHref } from "$lib/paraglide/runtime";
   import { Header } from "@repo/sveltekit-ui";
   import { routes } from "$lib/routes.js";
   import { session, authClient } from "@repo/library/better-auth";
@@ -13,3 +15,9 @@
 <Header title="NavNar Title" {routes} {session} {authClient}>
   {@render children()}
 </Header>
+
+<div style="display:none">
+  {#each locales as locale}
+    <a href={localizeHref(page.url.pathname, { locale })}>{locale}</a>
+  {/each}
+</div>
